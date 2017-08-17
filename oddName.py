49758df7ee64ac702e4c8b03fd35d15261e3c5e6
macca-lachlan <30332634+macca-lachlan@users.noreply.github.com>
2017-08-17 10:20:32 +1000

    complete oddName program

--- oddName.py
+++ oddName.py
@@ -2,4 +2,9 @@
 Lachlan McDonald
 """
 
-
+username = input("What is your name? :")
+while len(username) == 0:
+    print("You need to enter your name")
+    username = input("What is your name? :")
+for i in range(0, len(username), 2):
+    print(username[i])
